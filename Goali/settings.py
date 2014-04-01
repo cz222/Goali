124956c8c2a22dde127a56909a3d437575f46f22
MaggieBi <byunjie@gmail.com>
2014-04-01 19:58:36 -0400

    home page added

--- Goali/settings.py
+++ Goali/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = (
 #    'django.contrib.contenttypes',
 #    'django.contrib.sessions',
 #    'django.contrib.messages',
-#    'django.contrib.staticfiles',
+	'django.contrib.staticfiles',
 	'accounts',
 	'books',
 	'django.contrib.admin',
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
 	'django.contrib.contenttypes',
 	'django.contrib.messages',
 	'django.contrib.sessions',
+	'bootstrap3',
 )
 
 MIDDLEWARE_CLASSES = (
