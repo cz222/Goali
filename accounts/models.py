49f4e7f0beab7c0b458012254430342b3d0c05e0
unknown <kiwicz@hotmail.com>
2014-04-04 18:36:47 -0400

    Fixed Registration

--- accounts/models.py
+++ accounts/models.py
@@ -4,9 +4,8 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField(max_length=30)
-    email = models.EmailField()
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=75, unique=True)
+	
     def __unicode__(self):
         return self.username
 	
